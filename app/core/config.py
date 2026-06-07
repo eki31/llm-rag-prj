7c8f45a3075c058b1d29e75bbc8d48aa8d63ced4
eki31 <aqili1991@gmail.com>
2026-06-07 16:12:59 +0800

    Add memory persistence (last 5 conversation) that accidentally removed in past revert

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 import os
 
-#load_dotenv()
+load_dotenv()
 
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 OPENROUTER_MODEL = os.getenv("OPENROUTER_MODEL")
